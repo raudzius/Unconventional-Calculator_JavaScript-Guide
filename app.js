019dcd9d7916aeb45e9bd7a742d1f9f2d0ca4974
Mykolas Raudžius <raudziusmykolas@gmail.com>
2022-06-05 18:51:07 +0300

    feat: log infotmation about operation in console

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 let currentResult = 0;
 
+const logEntries = [];
+
 function createAndWriteOutput(currentResult, operator, userInputValue) {
   return `${currentResult} ${operator} ${userInputValue}`;
+}
+
+function writeToLog(operator, prevResult, userInput, result) {
+  const logEntry = {
+    opration: operator,
+    prevResult: prevResult,
+    userInput: +userInput,
+    result: result,
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
 }
 
 function add() {
@@ -9,6 +22,7 @@ function add() {
   currentResult += +userInput.value;
   let calculationDescription = createAndWriteOutput(prevResult, '+', userInput.value);
   outputResult(currentResult, calculationDescription);
+  writeToLog('+', prevResult, userInput.value, currentResult);
 }
 
 addBtn.addEventListener('click', add);
