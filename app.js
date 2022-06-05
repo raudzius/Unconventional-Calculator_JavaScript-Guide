9ff45f8b1e9267cef05e615581c5af50a32ac6f3
Mykolas Raudžius <raudziusmykolas@gmail.com>
2022-06-05 17:58:22 +0300

    feat: output calculation

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 let currentResult = 0;
 
+function createAndWriteOutput(currentResult, operator, userInputValue) {
+  return `${currentResult} ${operator} ${userInputValue}`;
+}
+
 function add() {
+  let prevResult = currentResult;
   currentResult += +userInput.value;
-  outputResult(currentResult);
+  let calculationDescription = createAndWriteOutput(prevResult, '+', userInput.value);
+  outputResult(currentResult, calculationDescription);
 }
 
 addBtn.addEventListener('click', add);
--- vendor.js
+++ vendor.js
@@ -1,7 +1,10 @@
 const userInput = document.getElementById('input-number');
 const addBtn = document.getElementById('btn-add');
-const currentResultOutput = document.getElementById('current-result');
 
-function outputResult(result) {
+const currentResultOutput = document.getElementById('current-result');
+const currentCalculationOutput = document.getElementById('current-calculation');
+
+function outputResult(result, text) {
   currentResultOutput.textContent = result;
+  currentCalculationOutput.textContent = text;
 }
